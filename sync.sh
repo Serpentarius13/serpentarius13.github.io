80db87ce79d9e38426bf5215c2cbff988f52702b
Serpentariu13 <serpentarium13@mail.ru>
2026-08-04 09:16:18 +0300

    chore: push with new obs html contents

--- sync.sh
+++ sync.sh
@@ -5,4 +5,4 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE="${SOURCE:-/home/shared/archlinuxenjoyer/articles to read.md}"
 
 python3 "${ROOT}/sync.py" "${SOURCE}"
-"${ROOT}/upgrade.sh"
+git add . && git commit -m 'chore: push with new obs html contents' && git push
